6eaee6b188d62ada20d578a66f8041ee4a615d4a
KngAlgo <chamboscott@gmail.com>
2025-08-07 01:09:52 +0000

    Finished Model Creation

--- CNN Classifier/model.py
+++ CNN Classifier/model.py
@@ -1,4 +1,9 @@
 import tensorflow as tf
+tf.config.experimental.enable_mlir_graph_optimization = False
+
+gpus = tf.config.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
 
 from tensorflow import keras
 from keras import layers, Model
@@ -17,20 +22,24 @@ x = layers.Conv2D(64, 3, activation='relu')(x)
 x = layers.MaxPooling2D(2, 2)(x)
 x = layers.Dropout(0.3)(x)
 
+x = layers.Conv2D(128, 3, activation='relu')(x)
+x = layers.MaxPooling2D(2, 2)(x)
+x = layers.Dropout(0.3)(x)
+
 x = layers.Flatten()(x)
 
 output = layers.Dense(53, activation='softmax')(x)
 
 model = keras.Model(input, output)
 
-train_dataset = keras.utils.image_dataset_from_directory('/workspace/CNN Classifier/images/train', batch_size=32)
+train_dataset = keras.utils.image_dataset_from_directory('/workspace/CNN Classifier/images/train', batch_size=16)
 
-test_dataset = keras.utils.image_dataset_from_directory('/workspace/CNN Classifier/images/test', batch_size=32)
+test_dataset = keras.utils.image_dataset_from_directory('/workspace/CNN Classifier/images/test', batch_size=16)
 
-val_dataset = keras.utils.image_dataset_from_directory('/workspace/CNN Classifier/images/valid', batch_size=32)
+val_dataset = keras.utils.image_dataset_from_directory('/workspace/CNN Classifier/images/valid', batch_size=16)
 
 checkpoint = keras.callbacks.ModelCheckpoint("card_model.keras", monitor='val_loss')
 
-model.compile(optimizer='adam', metrics=['accuracy'], loss="sparse_categorical_crossentropy", jit_compile=True)
+model.compile(optimizer='adam', metrics=['accuracy'], loss="sparse_categorical_crossentropy")
 
-model.fit(train_dataset, epochs=8, validation_data=val_dataset, callbacks=[checkpoint])
+model.fit(train_dataset, epochs=12, validation_data=val_dataset, callbacks=[checkpoint])
