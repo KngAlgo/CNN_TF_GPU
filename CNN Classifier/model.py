2fcc5dad4edd5c90449b9c6d2135c0f1e0f84888
KngAlgo <chamboscott@gmail.com>
2025-08-07 02:20:36 +0000

    Added small changes + Data Augmentation

--- CNN Classifier/model.py
+++ CNN Classifier/model.py
@@ -12,34 +12,52 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-input = keras.Input(shape=(256, 256, 3))
-x = layers.Rescaling(1./255)(input)
-x = layers.Conv2D(32, 3, activation='relu')(x)
-x = layers.MaxPooling2D(2, 2)(x)
-x = layers.Dropout(0.3)(x)
-
-x = layers.Conv2D(64, 3, activation='relu')(x)
-x = layers.MaxPooling2D(2, 2)(x)
-x = layers.Dropout(0.3)(x)
-
-x = layers.Conv2D(128, 3, activation='relu')(x)
-x = layers.MaxPooling2D(2, 2)(x)
-x = layers.Dropout(0.3)(x)
-
-x = layers.Flatten()(x)
-
-output = layers.Dense(53, activation='softmax')(x)
-
-model = keras.Model(input, output)
-
 train_dataset = keras.utils.image_dataset_from_directory('/workspace/CNN Classifier/images/train', batch_size=16)
 
 test_dataset = keras.utils.image_dataset_from_directory('/workspace/CNN Classifier/images/test', batch_size=16)
 
 val_dataset = keras.utils.image_dataset_from_directory('/workspace/CNN Classifier/images/valid', batch_size=16)
 
+data_aug = keras.Sequential(
+    [
+        layers.RandomFlip('horizontal'), 
+        layers.RandomZoom(0.2), 
+        layers.RandomRotation(0.1)
+        ]
+    )
+
+def make_model():
+    input = keras.Input(shape=(256, 256, 3))
+    x = data_aug(input)
+    x = layers.Rescaling(1./255)(x)
+    x = layers.Conv2D(32, 3, activation='relu')(x)
+    x = layers.MaxPooling2D(2, 2)(x)
+    x = layers.Dropout(0.3)(x)
+
+    x = layers.Conv2D(64, 3, activation='relu')(x)
+    x = layers.MaxPooling2D(2, 2)(x)
+    x = layers.Dropout(0.3)(x)
+    
+    x = layers.Conv2D(128, 3, activation='relu')(x)
+    x = layers.MaxPooling2D(2, 2)(x)
+    x = layers.Dropout(0.3)(x)
+    
+    x = layers.Flatten()(x)
+    
+    output = layers.Dense(53, activation='softmax')(x)
+    
+    model = keras.Model(input, output)
+
+    return model
+
+model_ = make_model()
+
 checkpoint = keras.callbacks.ModelCheckpoint("card_model.keras", monitor='val_loss')
 
-model.compile(optimizer='adam', metrics=['accuracy'], loss="sparse_categorical_crossentropy")
+model_.compile(optimizer='adam', metrics=['accuracy'], loss="sparse_categorical_crossentropy")
 
-model.fit(train_dataset, epochs=12, validation_data=val_dataset, callbacks=[checkpoint])
+model_.fit(train_dataset, epochs=12, validation_data=val_dataset, callbacks=[checkpoint])
+
+# # model = keras.load_model('card_model.keras')
+
+# pred = model.predict(test_dataset)
